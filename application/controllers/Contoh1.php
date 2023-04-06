6e9c930d10af37ebd0b04a39c94fd4c27470c799
gvrapatti <nyongpatti12.@gmail.com>
2023-04-06 12:44:01 +0700

    Tugas 3&4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+ {
+   echo "<h1>perkenalkan</h1>";
+   echo"Nama saya ferrari pattipeiluhu
+   Saya tingga di kota depok
+   olahraga yang saya sukai adalah
+   futsal
+   ";
+ }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ferrari</li>
+        <li>Nama Belakang : Pattipeiluhu</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>depok Kota</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>futsal</li>
+        <li>Sepakbola</li>
+    </ul>
+</section>
